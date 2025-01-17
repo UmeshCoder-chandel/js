cbe2131be3369d869d574059b05672fc3e31e0c5
Umesh Chandel <161915699+UmeshCoder-chandel@users.noreply.github.com>
2025-01-17 09:09:46 +0000

    datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,5 @@
+"use strict";//treat all js code as newer verion
+//alert(3+3)   // we are using node js, not browser
+
+console.log(3+3);  //code  readable should be high
+console.log("umesh")
---
+++ 01_basic/variable.js
@@ -0,0 +1,9 @@
+const accountId=15131
+let accountEmail="umdfsd@5416.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+//accountId=1  //not working because 
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
